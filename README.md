4dd55183a9473adcb939b30b8efc0e6c2ee65acc
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2024-11-05 20:29:04 +0530

    Edit files index.html, script.js, styles.css
    Edit files index.html, script.js, styles.css to add a prompt to let the
    user set the size of the grid.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-etch-a-sketch
+
+In this project I have created a sketchpad by using html css and javascript.
--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-const container = document.querySelector(".container");
-const squareDivArray = [];
-for(let i = 0; i < 256; i++){
-    squareDivArray[i] = document.createElement("div");
-    squareDivArray[i].classList.add("square-div");
-    container.appendChild(squareDivArray[i]);
+// Number of rows and columns of square divs is stored in a variable
+let gridSize = 16;
+
+function startGrid(){
+    const container = document.querySelector(".container");
+    container.innerHTML = "";
+    container.style.width = gridSize * 15 + 20 + "px";
+    const squareDivArray = [];
+    for(let i = 0; i < gridSize * gridSize; i++){
+        squareDivArray[i] = document.createElement("div");
+        squareDivArray[i].classList.add("square-div");
+        container.appendChild(squareDivArray[i]);
+    }
+    squareDivArray.forEach((item) => {
+        item.addEventListener("mouseenter", function(event) {
+            let target = event.target;
+            target.classList.add("square-div-hovered");
+        });
+    });
 }
 
-squareDivArray.forEach((item) => {
-    item.addEventListener("mouseenter", function(event) {
-        let target = event.target;
-        target.classList.add("square-div-hovered");
-    });
-});
+startGrid();
+
+const userInputButton = document.querySelector(".user-input-button");
+userInputButton.addEventListener("click", function(){
+    gridSizePrompt = prompt("Enter a number upto 100 to be set as grid size.", 0);
+    if(parseInt(gridSizePrompt) >= 0 && parseInt(gridSizePrompt) <= 100 ){
+        gridSize = parseInt(gridSizePrompt);
+    }
+    else {
+        alert("Invalid input! Click the button again and enter a number between 0 and 100.");
+    }
+    startGrid();
+})
